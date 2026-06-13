880a9ad7f0d319e55c437f171f706fc36189b82c
EmzarGu <70318292+EmzarGu@users.noreply.github.com>
2026-06-13 21:17:20 +0200

    Update default delivery fee

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,42 +1,81 @@
 from api.controllers import Delivery
 from django_mock_queries.query import MockSet, MockModel
 
-def test_LotsOfItems():
-  #Arrange
+
+def build_order(*quantities):
   order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
+  for quantity in quantities:
+    order.add(MockModel(quantity=quantity))
+  return order
+
+
+def test_LotsOfItems():
+  # Arrange
+  order = build_order(5, 5, 5)
   delivery_distance = 6
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
   assert cost == 7.5
 
+
 def test_MiddleOfTheRoadItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
+  # Arrange
+  order = build_order(2, 2, 2)
   delivery_distance = 4
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
   assert cost == 5
 
+
 def test_LittleItems():
-  #Arrange
-  # TODO: Arrange the items to run the test
-  order = MockSet()
-  order.add(MockModel(quantity=3))
-  order.add(MockModel(quantity=1))
-  del_distance = 2
-  
-  #Act
-  # TODO: Call the function that will be tested
-  cost = Delivery.calculate(order, del_distance)
-  #Assert
-  # TODO: replace the pass with an assert to test the value returned.
-  assert cost == 2.5
-  pass
+  # Arrange
+  order = build_order(3, 1)
+  delivery_distance = 2
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
+  assert cost == 3.5
+
+
+def test_MoreThanTenItemsButShortDistanceGetsMiddleDeliveryFee():
+  # Arrange
+  order = build_order(5, 5, 1)
+  delivery_distance = 4
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
+  assert cost == 5
+
+
+def test_ExactlyTenItemsAndLongDistanceGetsMiddleDeliveryFee():
+  # Arrange
+  order = build_order(5, 5)
+  delivery_distance = 6
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
+  assert cost == 5
+
+
+def test_ExactlyFiveItemsAndMiddleDistanceGetsBaseDeliveryFee():
+  # Arrange
+  order = build_order(2, 3)
+  delivery_distance = 4
+
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+
+  # Assert
+  assert cost == 3.5
